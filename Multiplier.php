438719d5ebe39a4dd3203768c80012d021b9ed96
Maro Balestracci <2109711@talnet.nl>
2019-10-02 14:26:59 +0200

    Uitwerking van de exercise

---
+++ Multiplier.php
@@ -0,0 +1,15 @@
+<?php
+echo "> Ik ga twee getallen met elkaar vermenigvuldigen" . PHP_EOL;
+echo "> Eerste getal?" . PHP_EOL;
+$line = readline("> ");
+if (!empty($line)) {
+  readline_add_history($line);
+}
+echo "> Tweede Getal?" . PHP_EOL;
+$bruh = readline("> ");
+if (!empty($bruh)) {
+  readline_add_history($bruh);
+}
+echo "> Uw resultaat is: ";
+echo ($line + $bruh);
+?>
